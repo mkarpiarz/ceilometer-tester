0fe118af1a265b3dcd59804a7792e95bbe781a4e
Mariusz Karpiarz <mariusz.karpiarz@datacentred.co.uk>
2017-06-03 10:45:00 +0100

    Use a keystone token for ceilometer.

--- show_billable_resources.py
+++ show_billable_resources.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python
 import os
+from keystoneauth1.identity import v3
+from keystoneauth1 import session
 import ceilometerclient.client
 
 def main():
     if not os.environ.get('OS_CEILOMETER_URL'):
         print("ERROR: $OS_CEILOMETER_URL variable not set!")
+        print("Use this as an example: OS_CEILOMETER_URL=https://compute.datacentred.io:8777")
         exit(1)
 
-    cclient = ceilometerclient.client.get_client(2,
-                               os_username=os.environ['OS_USERNAME'],
-                               os_password=os.environ['OS_PASSWORD'],
-                               os_tenant_name=os.environ['OS_PROJECT_NAME'],
-                               os_auth_url=os.environ['OS_AUTH_URL'],
-                               os_endpoint=os.environ['OS_CEILOMETER_URL'],
-                               user_domain_id='default',
-                               project_domain_id='default')
+    # get a token
+    auth = v3.Password(auth_url=os.environ['OS_AUTH_URL'],
+                       username=os.environ['OS_USERNAME'],
+                       password=os.environ['OS_PASSWORD'],
+                       project_name=os.environ['OS_PROJECT_NAME'],
+                       user_domain_id='default',
+                       project_domain_id='default')
+    sess = session.Session(auth=auth)
+    token = auth.get_token(sess)
 
+    # use the token with ceilometer's endpoint
+    cclient = ceilometerclient.client.get_client(2,
+                               os_endpoint=os.environ['OS_CEILOMETER_URL'],
+                               os_token=token)
+
+    print( cclient.alarms.list() )
+    exit(0)
     #print( cclient.samples.list() )
     query_samples = [dict(field='source', op='eq', value='instance')]
     print( cclient.samples.list( q=query_samples ) )
