b216adcab76fcfcaf2586b2f2cd543dc679883e6
Mariusz Karpiarz <mariusz.karpiarz@datacentred.co.uk>
2017-06-05 17:34:40 +0100

    Calculate the average time from the results.

--- benchmark_ceilometer.py
+++ benchmark_ceilometer.py
@@ -1,6 +1,7 @@
 import os
 import credentials
 import ceilomarius
+import numpy
 
 def main():
     if not os.environ.get('OS_CEILOMETER_URL'):
@@ -19,10 +20,15 @@ def main():
 
     query = [{'field': 'metadata.event_type', 'op': 'eq', 'value': 'compute.instance.exists'}]
     for l in xrange(1,11):
+        results = []
+        objects = []
         for iter in xrange(1,6):
             resp = ceilomar.get_metric(meter_name="instance", q=query, limit=l)
             objects = resp.json()
-            print("Objects retrieved: {}, us elapsed: {}, try no: {}".format(len(objects), resp.elapsed.microseconds, iter) )
+            elapsed = resp.elapsed.microseconds
+            print("Objects retrieved: {}, us elapsed: {}, try no: {}".format(len(objects), elapsed, iter) )
+            results.append(elapsed)
+        print( "Items: {}, avg response time: {}".format(len(objects), numpy.mean(results)) )
 
 if __name__ == "__main__":
     main()
