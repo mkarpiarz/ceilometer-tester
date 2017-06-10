053ea4cd51b80a6d8d6616a5ca0d63367dd032c2
Mariusz Karpiarz <mariusz.karpiarz@datacentred.co.uk>
2017-06-10 14:29:33 +0100

    Use the new class to return stack resources.

--- stack_resources.py
+++ stack_resources.py
@@ -1,6 +1,7 @@
 import os
 import credentials
 import heatclient.client
+import resource
 
 class Heat:
     def __init__(self):
@@ -21,14 +22,21 @@ class Heat:
         return self.heat.stacks.list()
 
     def get_resources_in_stack(self, stack_id):
-        return self.heat.resources.list(stack_id = stack_id)
+        # original resources from the stack
+        stack_resources = self.heat.resources.list(stack_id = stack_id)
+        # a list of simpler Resource objects
+        resources = [ resource.Resource(res.physical_resource_id, res.resource_type) for res in stack_resources ]
+        #return self.heat.resources.list(stack_id = stack_id)
+        return resources
 
 def main():
     heat = Heat()
     stacks = heat.get_stacks()
     for stack in stacks:
         print( stack )
-        print( heat.get_resources_in_stack(stack.id) )
+        resources = heat.get_resources_in_stack(stack.id)
+        for res in resources:
+            print res
 
 if __name__ == "__main__":
     main()
