665a9def0fd9d59c71f0cb777f0b60a203227d4d
Mariusz Karpiarz <mariusz.karpiarz@datacentred.co.uk>
2017-06-26 12:28:40 +0100

    Query events with curl + ask for instance:
    Add a call to the events API for retrieving an event.
    This also changes the query so it uses an instance ID instead of a project_id.
    
    TODO: Remove duplicated code.

--- ceilometer_get_response.sh
+++ ceilometer_get_response.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-if [ "$#" -eq 0 ];
+if [ "$#" -lt 2 ];
 then
-    echo "Usage: $0 <token_id>"
+    echo "Usage: $0 <token_id> <instance_id>"
     exit 1;
 fi
 
 set -x
 
 OS_TOKEN=$1
-OS_CEILOMETER_URL=https://compute.datacentred.io:8777
-if [ -z $OS_PROJECT_ID ];
+if [ -z $OS_CEILOMETER_URL ];
 then
-    echo "ERROR: Variable $OS_PROJECT_ID not set!"
-    exit 2
+    OS_CEILOMETER_URL=https://compute.datacentred.io:8777
+    echo "WARNING: '$OS_CEILOMETER_URL' not set, setting it to $OS_CEILOMETER_URL"
 fi
+
+INSTANCE_ID=$2
 
 curl -s -H "X-Auth-Token: $OS_TOKEN" \
     -X GET \
     -H "Content-Type: application/json" \
-    $OS_CEILOMETER_URL/v2/meters/instance \
-    -d '{ "q": [{"field": "project_id", "op": "eq", "value": "'$OS_PROJECT_ID'"}, {"field": "metadata.event_type", "op": "eq", "value": "compute.instance.exists"}], "limit": 1 }' \
+    $OS_CEILOMETER_URL/v2/events \
+    -d '{ "q": [{"field": "instance_id", "op": "eq", "value": "'$INSTANCE_ID'"}], "limit": 1 }' \
     | python -m json.tool
     #-d '{ "q": [{"field": "timestamp", "op": "ge", "value": "2017-06-01T00:00:00"}, {"field": "timestamp", "op": "lt", "value": "2017-06-01T00:10:00"}], "limit": 1 }' \
 echo
@@ -28,8 +29,17 @@ echo
 curl -s -H "X-Auth-Token: $OS_TOKEN" \
     -X GET \
     -H "Content-Type: application/json" \
-    -d '{ "q": [{"field": "project_id", "op": "eq", "value": "'$OS_PROJECT_ID'"}, {"field": "meter", "op": "eq", "value": "instance"}], "limit": 1 }' \
+    $OS_CEILOMETER_URL/v2/meters/instance \
+    -d '{ "q": [{"field": "resource_id", "op": "eq", "value": "'$INSTANCE_ID'"}, {"field": "metadata.event_type", "op": "eq", "value": "compute.instance.exists"}], "limit": 1 }' \
+    | python -m json.tool
+    #-d '{ "q": [{"field": "timestamp", "op": "ge", "value": "2017-06-01T00:00:00"}, {"field": "timestamp", "op": "lt", "value": "2017-06-01T00:10:00"}], "limit": 1 }' \
+echo
+
+curl -s -H "X-Auth-Token: $OS_TOKEN" \
+    -X GET \
+    -H "Content-Type: application/json" \
     $OS_CEILOMETER_URL/v2/samples \
+    -d '{ "q": [{"field": "resource_id", "op": "eq", "value": "'$INSTANCE_ID'"}, {"field": "meter", "op": "eq", "value": "instance"}], "limit": 1 }' \
     | python -m json.tool
     #-d '{ "q": [{"field": "timestamp", "op": "ge", "value": "2017-06-01T00:00:00"}, {"field": "timestamp", "op": "lt", "value": "2017-06-01T01:00:00"}] }' \
 echo
