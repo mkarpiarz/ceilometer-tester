4157053843ed628781769168332ee428357a5c61
Mariusz Karpiarz <mariusz.karpiarz@datacentred.co.uk>
2017-06-27 16:08:50 +0100

    Retrieve samples in pages:
    Implements fetching samples with paged queries. This approach seems to be way easier on our platform than just retrieving all the samples at once.

--- paginator.py
+++ paginator.py
@@ -14,11 +14,39 @@ class Paginator:
     def convert_date(self, date):
         return "{:%Y-%m-%dT%H:%M:%S.%f}".format(date)
 
+    def convert_timestamp(self, timestamp):
+        return datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%f")
+
     def get_meters_for_instance(self, instance_id, time_begin, time_end, limit = 100):
-        time_latest = time_end
-        print( "INFO: Latest timestamp on the page: {}".format(self.convert_date(time_latest)) )
-        query = [{"field": "resource_id", "op": "eq", "value": instance_id}, {"field": "timestamp", "op": "gt", "value": self.convert_date(time_begin)}, {"field": "timestamp", "op": "le", "value": self.convert_date(time_latest)}]
-        return self.ceilo.get_metric(meter_name="instance", q=query, limit=limit).json()
+        """Returns samples for specified instance divided into pages.
+
+        Attributes:
+        limit -- specifies the number of results on a page
+        """
+        # TODO: Make sure returned results are sorted by timestamps as expected
+        # and there is nothing missing missing between pages.
+
+        # the last timestamp on the list (the earliest)
+        time_earliest = time_end
+
+        # aggregate all retrieved samples in this list:
+        samples_all = []
+        while time_earliest >= time_begin:
+            print( "INFO: Latest timestamp on the page: {}".format(self.convert_date(time_earliest)) )
+            query = [{"field": "resource_id", "op": "eq", "value": instance_id}, {"field": "timestamp", "op": "ge", "value": self.convert_date(time_begin)}, {"field": "timestamp", "op": "lt", "value": self.convert_date(time_earliest)}]
+            #print( self.ceilo.get_statistics(meter_name="instance", q=query, count_only=True).json() )
+            samples = self.ceilo.get_metric(meter_name="instance", q=query, limit=limit).json()
+            # break if there are no more samples
+            if not samples:
+                break
+            # get the earliest timestamp
+            sample_earliest = samples[-1]
+
+            time_earliest = self.convert_timestamp( sample_earliest.get('timestamp') )
+            # now append samples from this page to the total list
+            samples_all += samples
+
+        return samples_all
 
 
 def main():
@@ -35,8 +63,9 @@ def main():
     t_begin = t_end - timedelta(days=10)
     print("INFO: Retrieving objects from {} to {}".format(pag.convert_date(t_begin), pag.convert_date(t_end)))
 
-    objects = pag.get_meters_for_instance(instance_id, t_begin, t_end)
-    print("INFO: Retrieved {} objects.".format(len(objects)))
+    samples = pag.get_meters_for_instance(instance_id, t_begin, t_end)
+    if samples:
+        print("INFO: Retrieved {} samples.".format(len(samples)))
 
 if __name__ == "__main__":
     main()
