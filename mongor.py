e13abb564a7affb736b9276190431e4a1cdde8f4
Mariusz Karpiarz <mariusz.karpiarz@datacentred.co.uk>
2017-06-28 12:14:44 +0100

    Get a list of collections in the database.

--- mongor.py
+++ mongor.py
@@ -8,10 +8,12 @@ class Mongor:
 
     def connect(self):
         self.client = pymongo.MongoClient(host = self.conf.host, port = int(self.conf.port))
+        db = self.client[self.conf.database]
+        db.authenticate(name=self.conf.username, password=self.conf.password, source=self.conf.database)
+        print( db.collection_names() )
 
 def main():
     mongor = Mongor()
-    print( mongor.client.server_info() )
 
 if __name__ == "__main__":
     main()
