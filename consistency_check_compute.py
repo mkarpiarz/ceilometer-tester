0df5494f7133f87523241fd767d942865b447b4b
Mariusz Karpiarz <mariusz.karpiarz@datacentred.co.uk>
2017-06-29 16:56:27 +0100

    [BUG?] Add logic for comparing lists of samples:
    This always returns one sample (the last one) as missing from the API, even though sizes of the lists match.
    Potentially a bug somewhere in the code.

--- consistency_check_compute.py
+++ consistency_check_compute.py
@@ -5,9 +5,41 @@ import argparse
 import credentials
 import config
 
-def compare_samples(samples_api, samples_db):
-    print( "INFO: Number of samples from API: {}".format(len(samples_api)) )
-    print( "INFO: Number of samples from the database: {}".format(len(samples_db)) )
+def compare_samples(samples_api, samples_db, verbose=False):
+    # First compare sizes of lists with samples
+    len_api = len(samples_api)
+    len_db = len(samples_db)
+    if verbose:
+        print( "INFO: Number of samples from API: {}".format(len_api) )
+        print( "INFO: Number of samples from the database: {}".format(len_db) )
+
+    if len_api == len_db:
+        print( "INFO: [SUCCESS] Numbers of samples ({}) match!".format(len_db) )
+    else:
+        print( "WARNING: [FAILURE] Number of samples from API ({}) and from the database {} don't match.".format(len_api, len_db) )
+
+    # Prepare a dictionary for samples from API
+    dict_samples_api = {}
+    for i in xrange( 0, len(samples_api) ):
+        dict_samples_api[samples_api[i]['message_id']] = i
+    print( len(dict_samples_api) )
+
+    # Go through the list of samples from the database
+    # and check which ones are not in the dict from API
+    # (also stores the dict of samples from db for reference)
+    dict_samples_db = {}
+    missing_in_api = []
+    for i in xrange( 0, len(samples_db) ):
+        if not dict_samples_api.get(samples_db[i]['message_id']):
+            missing_in_api.append(samples_db[i])
+        dict_samples_db[samples_db[i]['message_id']] = i
+    print( len(dict_samples_db) )
+
+    print( "INFO: Number of samples missing in the api: {}".format(len(missing_in_api)) )
+    if verbose:
+        print("INFO: Here are the missing items:")
+        print(missing_in_api)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -40,7 +72,7 @@ def main():
     samples_api = pag.get_samples_for_instance(instance_id, time_begin, time_end)
     samples_db = mon.get_samples_for_instance(instance_id, time_begin, time_end)
 
-    compare_samples(samples_api, samples_db)
+    compare_samples(samples_api, samples_db, verbose)
 
 if __name__ == "__main__":
     main()
