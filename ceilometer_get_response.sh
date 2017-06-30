985ede02b5603bc4b941d4d08eb05f279c1cef8b
Mariusz Karpiarz <mariusz.karpiarz@datacentred.co.uk>
2017-06-30 16:19:31 +0100

    Add a bash function to reduce duplicate code.

--- ceilometer_get_response.sh
+++ ceilometer_get_response.sh
@@ -6,7 +6,19 @@ then
     exit 1;
 fi
 
-set -x
+send_request() {
+    # $1: url
+    # $2: token
+    # $3: query
+    set -x
+    curl -s -H "X-Auth-Token: $2" \
+        -X GET \
+        -H "Content-Type: application/json" \
+        $1 \
+        -d "$3" \
+        | python -m json.tool
+    set +x
+}
 
 OS_TOKEN=$1
 if [ -z $OS_CEILOMETER_URL ];
@@ -14,34 +26,19 @@ then
     OS_CEILOMETER_URL=https://compute.datacentred.io:8777
     echo "WARNING: '$OS_CEILOMETER_URL' not set, setting it to $OS_CEILOMETER_URL"
 fi
-
 INSTANCE_ID=$2
 
-curl -s -H "X-Auth-Token: $OS_TOKEN" \
-    -X GET \
-    -H "Content-Type: application/json" \
-    $OS_CEILOMETER_URL/v2/events \
-    -d '{ "q": [{"field": "instance_id", "op": "eq", "value": "'$INSTANCE_ID'"}], "limit": 1 }' \
-    | python -m json.tool
-    #-d '{ "q": [{"field": "timestamp", "op": "ge", "value": "2017-06-01T00:00:00"}, {"field": "timestamp", "op": "lt", "value": "2017-06-01T00:10:00"}], "limit": 1 }' \
-echo
+# Get an event
+url=$OS_CEILOMETER_URL/v2/events
+query='{ "q": [{"field": "instance_id", "op": "eq", "value": "'$INSTANCE_ID'"}], "limit": 1 }'
+send_request $url $OS_TOKEN "$query"
 
-curl -s -H "X-Auth-Token: $OS_TOKEN" \
-    -X GET \
-    -H "Content-Type: application/json" \
-    $OS_CEILOMETER_URL/v2/meters/instance \
-    -d '{ "q": [{"field": "resource_id", "op": "eq", "value": "'$INSTANCE_ID'"}, {"field": "metadata.event_type", "op": "eq", "value": "compute.instance.exists"}], "limit": 1 }' \
-    | python -m json.tool
-    #-d '{ "q": [{"field": "timestamp", "op": "ge", "value": "2017-06-01T00:00:00"}, {"field": "timestamp", "op": "lt", "value": "2017-06-01T00:10:00"}], "limit": 1 }' \
-echo
+# Get a sample
+url=$OS_CEILOMETER_URL/v2/meters/instance
+query='{ "q": [{"field": "resource_id", "op": "eq", "value": "'$INSTANCE_ID'"}, {"field": "metadata.event_type", "op": "eq", "value": "compute.instance.exists"}], "limit": 1 }'
+send_request $url $OS_TOKEN "$query"
 
-curl -s -H "X-Auth-Token: $OS_TOKEN" \
-    -X GET \
-    -H "Content-Type: application/json" \
-    $OS_CEILOMETER_URL/v2/samples \
-    -d '{ "q": [{"field": "resource_id", "op": "eq", "value": "'$INSTANCE_ID'"}, {"field": "meter", "op": "eq", "value": "instance"}], "limit": 1 }' \
-    | python -m json.tool
-    #-d '{ "q": [{"field": "timestamp", "op": "ge", "value": "2017-06-01T00:00:00"}, {"field": "timestamp", "op": "lt", "value": "2017-06-01T01:00:00"}] }' \
-echo
-
-set +x
+# Get a sample with a different endpoint
+url=$OS_CEILOMETER_URL/v2/samples
+query='{ "q": [{"field": "resource_id", "op": "eq", "value": "'$INSTANCE_ID'"}, {"field": "meter", "op": "eq", "value": "instance"}], "limit": 1 }' \
+send_request $url $OS_TOKEN "$query"
